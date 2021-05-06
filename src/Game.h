5f07776e8df877e872ac5102bd26cbb26aa495c8
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2021-05-06 13:03:45 +0300

    init

---
+++ src/Game.h
@@ -0,0 +1,15 @@
+//
+// Created by User on 5/6/2021.
+//
+
+#ifndef AI_TETRIS_GAME_H
+#define AI_TETRIS_GAME_H
+#include<array>
+using namespace std;
+class Game {
+public:
+    array<int,200> ar = {1, 2, 3, 4, 5, 6};
+};
+
+
+#endif //AI_TETRIS_GAME_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include "Game.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "!!!!!!!!!!!!!!" << std::endl;
+    Game* a = new Game();
+    cout<<a->ar.max_size()<<endl;
+    cout<<a->ar.size()<<endl;
     return 0;
 }
